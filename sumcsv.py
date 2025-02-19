783b14bd15ce6bb4834d94762ab2eb164dd99439
JPocecco <pocecco@idrostudi.it>
2025-02-19 09:59:16 +0000

    Assistant checkpoint: Fixed indentation and syntax in sumcsv.py
    Assistant generated file changes:
    - sumcsv.py: Fix indentation and indexing syntax
    
    ---
    
    User prompt:
    
    IndentationError: unindent does not match any outer indentation level

--- sumcsv.py
+++ sumcsv.py
@@ -1,9 +1,10 @@
+
 def sum_csv(file_name):
- somma = 0
- my_file = open(file_name, 'r')
- for line in my_file:
-     elementi= line.split(', ')
-    somma = somma + elementi(2)
- return somma
+    somma = 0
+    my_file = open(file_name, 'r')
+    for line in my_file:
+        elementi = line.split(', ')
+        somma = somma + float(elementi[2])
+    return somma
     
 print(sum_csv('shampoo_sales.csv'))
