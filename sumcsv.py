2d61dcd73b21eb507f1d2c1aca3c2fd5cd3025bb
pocecco <39776160-pocecco@users.noreply.replit.com>
2025-02-19 09:58:44 +0000

    User checkpoint: Add shampoo sales data and function to calculate total sales.

---
+++ sumcsv.py
@@ -0,0 +1,9 @@
+def sum_csv(file_name):
+ somma = 0
+ my_file = open(file_name, 'r')
+ for line in my_file:
+     elementi= line.split(', ')
+    somma = somma + elementi(2)
+ return somma
+    
+print(sum_csv('shampoo_sales.csv'))
